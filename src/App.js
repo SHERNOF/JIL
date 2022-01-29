698098b0ba950e8127f0b595a48abb1124b0dce9
shernof <sherwin.nofuente@yahoo.com>
2022-01-29 23:54:57 +1100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>JESUS IS MY LORD!!!</h1>
+      <p>I'm His servant</p>
+    </div>
+  );
+}
+
+export default App;
